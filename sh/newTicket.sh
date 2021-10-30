336e5cba881e1e44a3d859e8a2ebd21bdbe1d9fb
krendeleno <krendeleno@gmail.com>
2021-10-30 21:43:47 +0300

    debug

--- sh/newTicket.sh
+++ sh/newTicket.sh
@@ -13,7 +13,7 @@ fi
 unique="https://github.com/krendeleno/hw8_infrastructure/$currentTag"
 description="$currentTag\n$author\n$date\n$gitlog"
 
-response=$(curl -s --write-out '%{http_code}' --request POST  "https://api.tracker.yandex.net/v2/issues/" -H "Content-Type: application/json" -H "Authorization: OAuth $OAuth" -H "X-Org-Id: $XOrgId" \
+response=$(curl -s --request POST  "https://api.tracker.yandex.net/v2/issues/" -H "Content-Type: application/json" -H "Authorization: OAuth $OAuth" -H "X-Org-Id: $XOrgId" \
     -d '{
     "summary": "New release "'"$currentTag"'",
     "queue": "TMP",
@@ -22,10 +22,12 @@ response=$(curl -s --write-out '%{http_code}' --request POST  "https://api.track
     "unique": "'"$unique"'"
     }')
 
-if [ $response = 201 ]; then
-  echo "Release created successfully"
-  elif [ $response = 404 ]; then
-  echo "Not found"
-  elif [ $response = 409 ]; then
-  ehcho "Can't create release with the same unique"
-fi
+echo $response
+
+#if [ $response = 201 ]; then
+#  echo "Release created successfully"
+#  elif [ $response = 404 ]; then
+#  echo "Not found"
+#  elif [ $response = 409 ]; then
+#  ehcho "Can't create release with the same unique"
+#fi
