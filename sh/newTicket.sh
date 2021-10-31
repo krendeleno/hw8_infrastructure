3a63f98554dd9b2f94d32aef197bfd12daf68149
krendeleno <krendeleno@gmail.com>
2021-10-31 13:28:17 +0300

    another test7

--- sh/newTicket.sh
+++ sh/newTicket.sh
@@ -5,9 +5,9 @@ prevTag=$(git tag | tail -2 | head -1)
 author=$(git show $currentTag  --pretty=format:"Author: %an" --date=format:'%Y-%m-%d %H:%M:%S' --no-patch)
 date=$(git show $currentTag  --pretty=format:"Date: %ad" --date=format:'%Y-%m-%d %H:%M:%S'  --no-patch)
 if [ $currentTag = $prevTag ]; then
-    gitlog=$(git log $prevTag..$currentTag --pretty=format:"\n* %h %an %ad %s" --date=format:'%Y/%m/%d%H:%M:%S')
+    gitlog=$(git log $prevTag..$currentTag --pretty=format:"\n* %h %an %ad %s" --date=format:'%Y/%m/%d-%H:%M:%S')
 else
-    gitlog=$(git log $prevTag..$currentTag --pretty=format:"\n* %h %an %ad %s" --date=format:'%Y/%m/%d%H:%M:%S')
+    gitlog=$(git log $prevTag..$currentTag --pretty=format:"\n* %h %an %ad %s" --date=format:'%Y/%m/%d-%H:%M:%S')
 fi
 
 unique="https://github.com/krendeleno/hw8_infrastructure/$currentTag"
@@ -16,7 +16,7 @@ summary="New release $currentTag from github.com/krendeleno/hw8_infrastructure"
 
 
 response=$(
-  curl -s -X POST https://api.tracker.yandex.net/v2/issues \
+  curl -s -o dev/null -w '%{http_code}' -X POST https://api.tracker.yandex.net/v2/issues \
   -H "Content-Type: application/json" \
   -H "Authorization: OAuth ${OAuth}" \
   -H "X-Org-Id: ${XOrgId}" \
@@ -31,40 +31,40 @@ response=$(
 
 echo $response
 
-#if [ $response = 201 ]; then
-#  echo "Release created successfully"
-#elif [ $response = 404 ]; then
-#  echo "Not found"
-#elif [ $response = 409 ]; then
-#  echo "Can't create release with the same unique"
+if [ $response = 201 ]; then
+  echo "Release created successfully"
+elif [ $response = 404 ]; then
+  echo "Not found"
+elif [ $response = 409 ]; then
+  echo "Can't create release with the same unique"
 
-#  taskID=$(
-#    curl -s -X POST https://api.tracker.yandex.net/v2/issues/_search? \
-#    -H "Content-Type: application/json" \
-#    -H "Authorization: OAuth $OAuth" \
-#    -H "X-Org-Id: $XOrgId" \
-#    -d '{
-#    "filter": {
-#         "unique":"'"$unique"'"
-#      }
-#    }' | jq -r '.[].id'
-#  )
-#
-#    updateResponse=$(
-#    curl -s -o dev/null -w '%{http_code}' -X PATCH https://api.tracker.yandex.net/v2/issues/$taskID \
-#    -H "Content-Type: application/json" \
-#    -H "Authorization: OAuth $OAuth" \
-#    -H "X-Org-Id: $XOrgId" \
-#    -d '{
-#        "summary":"'"$summary"'",
-#        "description":"'"$description"'"
-#    }')
-#
-#    if [ $updateResponse = 200 ]; then
-#      echo "Release updated successfully"
-#    elif [ $updateResponse = 404 ]; then
-#      echo "Not found"
-#    else [ $updateResponse = 409 ]
-#      echo "Something went wrong with statusCode: $updateResponse"
-#    fi
-#fi
+  taskID=$(
+    curl -s -X POST https://api.tracker.yandex.net/v2/issues/_search? \
+    -H "Content-Type: application/json" \
+    -H "Authorization: OAuth $OAuth" \
+    -H "X-Org-Id: $XOrgId" \
+    -d '{
+    "filter": {
+         "unique":"'"$unique"'"
+      }
+    }' | jq -r '.[].id'
+  )
+
+    updateResponse=$(
+    curl -s -o dev/null -w '%{http_code}' -X PATCH https://api.tracker.yandex.net/v2/issues/$taskID \
+    -H "Content-Type: application/json" \
+    -H "Authorization: OAuth $OAuth" \
+    -H "X-Org-Id: $XOrgId" \
+    -d '{
+        "summary":"'"$summary"'",
+        "description":"'"$description"'"
+    }')
+
+    if [ $updateResponse = 200 ]; then
+      echo "Release updated successfully"
+    elif [ $updateResponse = 404 ]; then
+      echo "Not found"
+    else [ $updateResponse = 409 ]
+      echo "Something went wrong with statusCode: $updateResponse"
+    fi
+fi
